36d0c53f5db42ca47e7732087d84615de29fdd28
Mamontov Dmitry <mamontov.dp@gmail.com>
2016-09-09 11:56:14 +0300

    Added Boost dependency  - we need to support unordered_map for old implementations.

---
+++ include/dukpp-03.h
@@ -0,0 +1,10 @@
+/*! \file dukpp-03.h
+
+    A basic file, which should be included to work with DukPP-03
+ */
+
+#ifndef DUKPP03_SADDY
+    #include <boost/unordered/unordered_map.hpp>
+#else
+    #include "../../boost-dist/boost/unordered/unordered_map.hpp"
+#endif
