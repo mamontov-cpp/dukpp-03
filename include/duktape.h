67a4c6809a7f36c30e2a88b7f10046f451fa6a0f
Mamontov Dmitry <mamontov.dp@gmail.com>
2016-09-09 11:48:46 +0300

    Added basic layout and Duktape 1.5.1 as a basis for further implementation.

---
+++ include/duktape.h
@@ -0,0 +1,6 @@
+/*! \file duktape.h
+    Common header for including duktape
+ */
+#pragma once
+
+#include "../duktape/src/duktape.h"
---
+++ src/duktape.cpp
@@ -0,0 +1,4 @@
+#ifdef _MSC_VER
+    #pragma warning(disable: 4551)
+#endif
+#include "../duktape/src/duktape.c"
