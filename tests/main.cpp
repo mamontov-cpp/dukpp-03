b8467dd3f3cb5e486ad3cdee75d75f43f9d2b285
Mamontov Dmitry <mamontov.dp@gmail.com>
2016-09-20 11:18:26 +0300

    Added tests skeleton.

---
+++ tests/main.cpp
@@ -0,0 +1,18 @@
+#pragma warning(push)
+#pragma warning(disable: 4273)
+#pragma warning(disable: 4351)
+#include <cstdio>
+#define _INC_STDIO
+#include "include/3rdparty/tpunit++/tpunit++.hpp"
+#pragma warning(pop)
+
+int main(int argc, char** argv)
+{
+    /**
+     * Run all of the registered tpunit++ tests. Returns 0 if
+     * all tests are successful, otherwise returns the number
+     * of failing assertions.
+     */
+    int result = tpunit::Tests::Run();
+    return 0;
+}
