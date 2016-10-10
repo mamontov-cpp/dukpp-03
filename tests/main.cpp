94293d52d5b19a06be2d4781f99f31e9f21a046f
Mamontov Dmitry <mamontov.dp@gmail.com>
2016-10-10 17:53:27 +0300

    Added interactive testing capabilities for further research.

--- tests/main.cpp
+++ tests/main.cpp
@@ -2,19 +2,44 @@
 #pragma warning(disable: 4273)
 #pragma warning(disable: 4351)
 #include <cstdio>
+#include <iostream>
 #include "context.h"
 #define _INC_STDIO
 #include "include/3rdparty/tpunit++/tpunit++.hpp"
 #pragma warning(pop)
 
+//#define TEST_INTERACTIVE
 int main(int argc, char** argv)
 {
-	dukpp03::context::Context t;
+#ifdef TEST_INTERACTIVE
+    dukpp03::context::Context t;
+    bool exit = false;
+    std::cout << "Interactive test shell. Type \"quit\" to quit\n";
+    do 
+    {
+        std::cout << "test> ";
+        std::string test, error;
+        std::getline(std::cin, test);
+        if (test == "quit")
+        {
+            exit = true;
+        }
+        else
+        {
+            bool res = t.eval(test, false, &error);
+            if (!res)
+            {
+                std::cout << error << "\n";
+            }
+        }
+    } while(!exit);
+#else
     /**
      * Run all of the registered tpunit++ tests. Returns 0 if
      * all tests are successful, otherwise returns the number
      * of failing assertions.
      */
-    int result = tpunit::Tests::Run();
+    int result = tpunit::Tests::Run();	
+#endif
     return 0;
 }
