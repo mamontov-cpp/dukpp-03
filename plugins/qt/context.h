c7e607c59b94e3b84c75f2edc9459a5529021348
Mamontov Dmitry <mamontov.dp@gmail.com>
2016-10-13 16:18:04 +0300

    Added GetValue instantiation for Qt values.

--- plugins/qt/context.h
+++ plugins/qt/context.h
@@ -20,4 +20,39 @@ typedef dukpp03::Context<dukpp03::qt::MapInterface, dukpp03::qt::VariantInterfac
 
 }
 
+
+template<>
+class GetValue<QVariant, dukpp03::qt::Context>
+{
+public:
+/*! Performs getting value from stack 
+    \param[in] ctx context
+    \param[in] pos index for stack
+    \return a value if it exists, otherwise empty maybe
+ */
+inline static dukpp03::Maybe<QVariant> perform(
+    dukpp03::qt::Context* ctx, 
+    duk_idx_t pos
+)
+{
+    dukpp03::Maybe<QVariant> result;
+    if (duk_is_object(ctx->context(), pos))
+    {
+       duk_get_prop_string(ctx->context(), pos, DUKPP03_VARIANT_PROPERTY_SIGNATURE);
+        if (duk_is_pointer(ctx->context(), -1))
+        {
+            void* ptr = duk_to_pointer(ctx->context(), -1);
+            QVariant * v = reinterpret_cast<QVariant *>(ptr);
+            if (v)
+            {
+                result.setValue(*v);
+            }
+        }
+        duk_pop(ctx->context());
+    }
+    return result;
+}
+
+};
+
 }
