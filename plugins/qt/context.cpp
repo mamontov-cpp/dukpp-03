dcc414bdf52148b8f76d684505bb0a3fc997cecf
Дмитрий Мамонтов <mamontov@intervolga.ru>
2016-10-18 18:10:02 +0300

    Fix for removing function.

--- plugins/qt/context.cpp
+++ plugins/qt/context.cpp
@@ -30,11 +30,6 @@ dukpp03::qt::Context::Context()
 	{
 		qRegisterMetaType<QPair<QObject*, dukpp03::qt::ValueOwnership> >("QPair<QObject*, dukpp03::qt::ValueOwnership>");
 	}
-	// Provice global finalizer functions to garbage collect and finalize QObjects
-	duk_push_global_object(m_context);
-	duk_push_c_function(m_context, dukpp03::qt::qobjectfinalizer, 1);
-	duk_def_prop(m_context, -3, DUK_DEFPROP_HAVE_VALUE | DUK_DEFPROP_HAVE_WRITABLE | 0);
-	duk_pop(m_context); 
 }
 
 dukpp03::qt::Context::~Context()
