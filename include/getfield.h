55125f26c52c53f5ec93f690fe068e0ec517ed6d
Дмитрий Мамонтов <mamontov@intervolga.ru>
2017-02-10 16:34:05 +0300

    Added ability to remap fields to other classes.

--- include/getfield.h
+++ include/getfield.h
@@ -70,6 +70,65 @@ protected:
 };
 
 
+
+
+/*! Defines a wrapper for getting field of class
+ */
+template<
+    typename _Context,
+	typename _DestClassName,
+    typename _ClassName,
+    typename _ReturnType
+>
+class GetFieldFor : public dukpp03::FunctionCallable<_Context>
+{
+public:
+    /*! A function type, which is being wrapped
+     */
+    typedef _ReturnType _ClassName::*Field;
+    /*! Constructs new function wrapper
+        \param[in] f function
+     */
+    GetFieldFor(Field f): m_field(f)   
+    {
+    }
+    /*! Returns copy of callable object
+        \return copy of callable object
+     */
+    virtual dukpp03::Callable<_Context>* clone()  
+    {
+        return new dukpp03::GetFieldFor<_Context, _DestClassName, _ClassName, _ReturnType>(m_field);
+    }
+    /*! Returns count of required arguments
+        \return count of required arguments
+     */
+    virtual int requiredArguments()
+    {
+        return 0;
+    }
+
+    DUKPP03_CBC_WITH_THIS_0(_DestClassName)
+    /*! Performs call of object, using specified context
+        \param[in] c context
+        \return count of values on stack, placed by functions
+     */
+    virtual int _call(_Context* c)
+    {
+        DUKPP03_MAYBE_FROM_THIS(_DestClassName);
+        dukpp03::PushValue<_ReturnType, _Context>::perform(c, ((_ac._()).*m_field));
+        return 1;
+    }
+    /*! Can be inherited
+     */
+    virtual ~GetFieldFor()
+    {
+    }
+protected:
+    /*! A field, which is being wrapped
+     */
+    Field m_field;
+};
+
 /*! A convenient wrapper for typedefs
  */ 
 template<typename _Context> 
@@ -84,6 +143,25 @@ struct getter
     {
         return new dukpp03::GetField<_Context, _ClassName, _ReturnType>(field);
     }
+
+/*! Remaps a field to specified class
+ */
+template<
+	typename _DestClass
+>
+struct to
+{
+    /*! Returns a getter for specified field
+        \param[in] field a field
+        \return getter
+     */
+    template<typename _ClassName, typename _ReturnType>
+    static dukpp03::GetFieldFor<_Context, _DestClass,  _ClassName, _ReturnType>* from(_ReturnType _ClassName::*field)
+    {
+        return new dukpp03::GetFieldFor<_Context, _DestClass,  _ClassName, _ReturnType>(field);
+    }	    
+};
+
 };
 
 };
--- include/setfield.h
+++ include/setfield.h
@@ -13,7 +13,7 @@
 namespace dukpp03
 {
 
-/*! Defines a wrapper for getting field of class
+/*! Defines a wrapper for setting field of class
  */
 template<
     typename _Context, 
@@ -71,6 +71,66 @@ protected:
 };
 
 
+
+/*! Defines a wrapper for setting field of class
+ */
+template<
+    typename _Context, 
+    typename _DestClassName, 
+    typename _ClassName,
+    typename _ReturnType
+>
+class SetFieldFor : public dukpp03::FunctionCallable<_Context>
+{
+public:
+    /*! A function type, which is being wrapped
+     */
+    typedef _ReturnType _ClassName::*Field;
+    /*! Constructs new function wrapper
+        \param[in] f function
+     */
+    SetFieldFor(Field f): m_field(f)   
+    {
+    }
+    /*! Returns copy of callable object
+        \return copy of callable object
+     */
+    virtual dukpp03::Callable<_Context>* clone()  
+    {
+        return new dukpp03::SetFieldFor<_Context, _DestClassName, _ClassName, _ReturnType>(m_field);
+    }
+    /*! Returns count of required arguments
+        \return count of required arguments
+     */
+    virtual int requiredArguments()
+    {
+        return 1;
+    }
+
+    DUKPP03_CBC_WITH_THIS_1(_DestClassName, _ReturnType)
+    /*! Performs call of object, using specified context
+        \param[in] c context
+        \return count of values on stack, placed by functions
+     */
+    virtual int _call(_Context* c)
+    {
+        DUKPP03_MAYBE_FROM_THIS(_DestClassName);
+        DUKPP03_MAYBE_FROM_STACK(_ReturnType, 0, 0, 1);
+        ((_ac._()).*m_field) = _a0._();
+        return 0;
+    }
+    /*! Can be inherited
+     */
+    virtual ~SetFieldFor()
+    {
+    }
+protected:
+    /*! A field, which is being wrapped
+     */
+    Field m_field;
+};
+
+
 /*! A convenient wrapper for typedefs
  */ 
 template<typename _Context> 
@@ -85,6 +145,26 @@ struct setter
     {
         return new dukpp03::SetField<_Context, _ClassName, _ReturnType>(field);
     }
+
+
+/*! Remaps a field to specified class
+ */
+template<
+	typename _DestClass
+>
+struct to
+{
+    /*! Returns a getter for specified field
+        \param[in] field a field
+        \return getter
+     */
+    template<typename _ClassName, typename _ReturnType>
+    static dukpp03::SetFieldFor<_Context, _DestClass,  _ClassName, _ReturnType>* from(_ReturnType _ClassName::*field)
+    {
+        return new dukpp03::SetFieldFor<_Context, _DestClass,  _ClassName, _ReturnType>(field);
+    }
+};
+
 };
 
 };
