ecc166804c1dac3f939d6466c7b63ce1e3e721bf
Mamontov Dmitry <mamontov.dp@gmail.com>
2017-03-09 10:29:42 +0300

    Fixes for builds with MinGW

--- duktape/src/duk_config.h
+++ duktape/src/duk_config.h
@@ -3423,5 +3423,5 @@ typedef struct duk_hthread duk_context;
 #endif  /* defined(DUK_USE_BYTEORDER) */
 
 #endif  /* DUK_CONFIG_H_INCLUDED */
-#include "../duktape/extras/print-alert/duk_print_alert.h"
-#include "../duktape/extras/duk-v1-compat/duk_v1_compat.h"
+#include "../extras/print-alert/duk_print_alert.h"
+#include "../extras/duk-v1-compat/duk_v1_compat.h"
