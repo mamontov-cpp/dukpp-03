4ffce55eeda2bc9c41b1a506298c1703a645c746
Mamontov <mamontov.dp@gmail.com>
2017-09-04 23:42:32 +0300

    [CORE] First attempt to refactor a JSObject.

--- include/jsobject.h
+++ include/jsobject.h
@@ -1,19 +1,26 @@
 #include "object.h"
 #include "context.h"
 #include <vector>
-#include <utility>
 #include <stdexcept>
 
 namespace dukpp03
 {
 
 template<typename _Context>
-class JSObject: public dukpp03::Object
+class JSObject
 {
 public:
+    /*! A link for storing link of object to context
+     */
+    struct Link
+    {
+        _Context* Context;     //!< A linked context
+        void* HeapPointer;     //!< A heap pointer
+        unsigned int RefCount; //!< A reference counter data
+    };
     /*! Makes new empty object
      */
-    JSObject() : m_is_null(false)
+    JSObject() : m_refcount(0), m_is_null(false)
     {
         
     }
@@ -29,20 +36,18 @@ public:
 	    \param[in] ctx context
 	    \param[in] name a name of object as field
 	 */
-	virtual void registerIn(dukpp03::AbstractContext* ctx, const std::string& name)
-    {
-        throw new std::logic_error("This context is incompatible with this object");
-    }
-
-
-    /*! Registers object as global in some context
-	    \param[in] ctx context
-	    \param[in] name a name of object as field
-	 */
 	virtual void registerIn(_Context* ctx, const std::string& name)
     {
-        m_registered_in.push_back(std::make_pair(ctx, name));
-        this->dukpp03::Object::registerIn(ctx, name);
+        bool found = false;
+        for(size_t i = 0; i < m_links.size(); i++)
+        {
+            if (m_links[i].Context == ctx)
+            {
+                found = true;
+                ++m_links[i].RefCount;
+            }
+        }
+        // TODO: Actually implement registering
     }
 
 	/*! Adds new property to object. If property exists, replaces it
@@ -82,7 +87,6 @@ public:
         {
             throw new std::logic_error("Attempted to add property to a null object");
         }
-	    throw new std::logic_error("dukpp03::Object cannot be placed into dukpp03::JSObject");
 	}
 	/*! Adds new property to object. If property exists, replaces it
 	    \param[in] name a name of property
@@ -102,17 +106,36 @@ public:
 	 */
 	virtual void removeProperty(const std::string& name)
     {
-        this->dukpp03::Object::removeProperty(name);
-        for(size_t i = 0; i < m_registered_in.size(); i++)
+        if (m_is_null)
         {
-            duk_context* c = m_registered_in[i].first;
-            std::string parentname = m_registered_in[i].second;
-            std::string rname = "delete ";
-            rname += parentname;
-            rname += ".";
-            rname += name;
-            rname += ";";
-            duk_eval_string(c, rname.c_str());
+            throw new std::logic_error("Attempted to remove property to a null object");
+        }
+        for(size_t i = 0; i < m_links.size(); i++)
+        {
+            duk_context* c = m_links[i].Context->context();
+            duk_push_heapptr(c, m_links[i].HeapPointer);
+            duk_del_prop_string(c, -1, name.c_str());
+            duk_pop(c);
+        }
+        // TODO: Field-dependent removal
+        // TODO: Remove field from arrays
+    }
+
+    /*! Adds reference to object
+     */
+    void addRef()
+    {
+        ++m_refcount;
+    }
+
+    /*! Deletes reference from object
+     */
+    void delRef()
+    {
+        --m_refcount;
+        if (m_refcount <= 0)
+        {
+            delete this;
         }
     }
 
@@ -122,20 +145,16 @@ public:
         result->m_is_null = true;
         return result;
     }
-
-    /*! A names list for an object
-        \return names of object in various context
-     */
-    const std::vector<std::pair<_Context*, std::string> >& names()
-    {
-        return m_registered_in;
-    }
-
 protected:
-
     /*! A contexts with name where object is registered
      */
-    std::vector<std::pair<_Context*, std::string> > m_registered_in;
+    std::vector<typename dukpp03::JSObject<_Context>::Link> m_links;
+    /*! A list of parent objects
+     */
+    std::vector<dukpp03::JSObject<_Context>* > m_parent_objects;
+    /*! A reference counting
+     */
+    unsigned int m_refcount;
     /*! Whether we should push null value for object
      */
     bool m_is_null;
